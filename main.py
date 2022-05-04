9af992cb89edc3b0c83ea2c7260d1a44f168a221
GraemeBrabe Barbe <99273811+GraemeBarbe@users.noreply.github.com>
2022-05-04 18:27:58 +0000

    index.python

---
+++ main.py
@@ -0,0 +1,17 @@
+age = int(input("Please enter your age: "))
+day = input("Please enter the day: ")
+day = day.upper()
+
+if (day == "MONDAY") or (day == "TUESDAY") or (day == "WEDNESDAY") or (day == "THURSDAY") or (day == "FRIDAY") or (day == "SATURDAY") or (day == "SUNDAY"):
+	if (age <= 5) or (age >= 95):
+		print("You can get in for free!")
+	elif (age >= 12) and (age <= 21):
+		print("You get a student discount!")
+	elif (day == "TUESDAY"):
+		print("Because it's tuesday you get a student discount!")
+	elif (day == "THURSDAY"):
+		print("Because it's thursday you get a student discount!")
+	else:
+		print("You need to pay full price")
+else:
+	print("Thats not a real day")
